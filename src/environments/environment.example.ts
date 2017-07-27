9c5cb9f9f35730ed96de3bd82acf272914eaed78
xdzeta <yulia.grischenko@gmail.com>
2017-07-27 19:25:05 +0300

    add example of env config

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "apiKey",
+        authDomain: "example.com",
+        databaseURL: "http://db....com",
+        projectId: "projectId",
+        storageBucket: "storageBucket",
+        messagingSenderId: "12345"
+    }
+};
